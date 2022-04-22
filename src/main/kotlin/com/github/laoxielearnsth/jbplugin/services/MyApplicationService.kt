e00bf283a5d39abffe09b9dbce7c61916dc0037a
GitHub Action <action@github.com>
2022-04-22 13:52:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/laoxielearnsth/jbplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/laoxielearnsth/jbplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.laoxielearnsth.jbplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.laoxielearnsth.jbplugin.MyBundle
 
 class MyApplicationService {
 
